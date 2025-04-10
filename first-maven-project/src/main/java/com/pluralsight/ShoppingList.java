c21288a927fe64ad650eff644334892678eda228
Camille Ingram <camille_d_ingram@live.com>
2025-04-10 10:39:36 -0400

    complete ShoppingList app

---
+++ first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,7 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Milk, Cheese, Water, Chips, Pizza, Bread, Ice Cream, Chicken, Pork, Rice");
+    }
+}
