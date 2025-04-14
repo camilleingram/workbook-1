e7721c5b633a1fcd7de649cbba32d3912fe566f9
Camille Ingram <camille_d_ingram@live.com>
2025-04-14 12:50:36 -0400

    add loaded option for the sandwich shop

--- SandwichShop/src/main/java/com/pluralsight/SandwichShop.java
+++ SandwichShop/src/main/java/com/pluralsight/SandwichShop.java
@@ -8,6 +8,7 @@ public class SandwichShop {
     public static void main(String[] args) {
 
         int sandwichSize, age;
+        String loaded;
 
         System.out.println("1: Regular: base price $5.45");
         System.out.println("2: Large: base price $8.95");
@@ -15,35 +16,39 @@ public class SandwichShop {
         sandwichSize = scanner.nextInt();
         scanner.nextLine();
 
+        System.out.print("Would you like the sandwich loaded? (yes/no) ");
+        loaded = scanner.nextLine();
+
+
         System.out.print("How old are you? ");
         age = scanner.nextInt();
         scanner.nextLine();
 
-        discount(sandwichSize, age);
+        handleTotal(sandwichSize, age, loaded);
 
         scanner.close();
     }
 
     public static double sandwichPrice (int sandwichSize) {
+        double price = 0;
 
         if (sandwichSize == 1) {
-            return 5.45d;
-        } else {
-            return 8.95d;
+            price = 5.95;
+        } else if (sandwichSize == 2){
+            price = 8.95;
         }
-
+        return price;
     }
 
-    public static void discount(int sandwichSize, int age) {
+    public static void handleTotal(int sandwichSize, int age, String loaded) {
 
-        double price = sandwichPrice(sandwichSize);
+        double price = handleAddOn(sandwichSize, loaded);
         double discount;
 
         if (age <= 17){
             discount = 0.1;
             price -= (price * discount);
             System.out.printf("Student discount added! Your total is $%.2f", price);
-
 
         } else if (age >= 65) {
             discount = 0.2;
@@ -53,6 +58,24 @@ public class SandwichShop {
         } else {
             System.out.printf("No discount added. Your total is $%.2f", price);
         }
+    }
+
+    public static double handleAddOn(int sandwichSize, String loaded) {
+        double price = sandwichPrice(sandwichSize);
+        double addOn;
+
+        if (sandwichSize == 1 && loaded.equals("yes")) {
+            addOn = 1;
+            price += addOn;
+        } else if(sandwichSize == 2 && loaded.equals("yes")) {
+            addOn = 1.75;
+            price += addOn;
+        } else if ((sandwichSize == 1 && loaded.equals("no") || sandwichSize == 2 && loaded.equals("no"))) {
+            addOn = 0;
+            price += addOn;
+        }
+        
+        return price;
     }
 
 }
